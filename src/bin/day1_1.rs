40644199a157a0d04d00b70fa04abe4d277cabcf
Niko Kivel <niko.kivel@lightsource.ca>
2024-12-01 20:41:24 -0600

    day1, part 1, with data

--- src/bin/day1_1.rs
+++ src/bin/day1_1.rs
@@ -4,24 +4,31 @@ mod advent_of_code;
 use std::cmp::max;
 use std::cmp::min;
 
+// There is no build in `abs` for u32 in Rust, who would have guessed that.
 fn absolute_difference_u32(a: &u32, b: &u32) -> u32 {
     max(a, b) - min(a, b)
 }
 
+// The puzlle calls for two lists (given as two columns in a ascii file) to be sorted and line by line the absolute differences need to be summed up.
 fn day1_1(data: &Vec<String>) -> u32 {
-    let mut col1: Vec<u32> = Vec::new();
-    let mut col2: Vec<u32> = Vec::new();
+    // two empty lists
+    let mut left_list: Vec<u32> = Vec::new();
+    let mut right_list: Vec<u32> = Vec::new();
+    // split each input at the whitespace and push the values into the lists.
     data.iter().for_each(|l| {
         let mut parts = l.split_whitespace();
         let num1 = parts.next().unwrap().parse::<u32>().unwrap();
         let num2 = parts.next().unwrap().parse::<u32>().unwrap();
-        col1.push(num1);
-        col2.push(num2);
+        left_list.push(num1);
+        right_list.push(num2);
     });
-    col1.sort();
-    col2.sort();
-    col1.iter()
-        .zip(col2.iter())
+    // sort the lists.
+    left_list.sort();
+    right_list.sort();
+    // iterate over both lists, map the absolute difference funtion onto each element and buld the sum
+    left_list
+        .iter()
+        .zip(right_list.iter())
         .map(|(x, y)| absolute_difference_u32(x, y))
         .sum()
 }
