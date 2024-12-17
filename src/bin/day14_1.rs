9797a919813a7ae85decb5fdac5a552ec9d63b93
Niko Kivel <niko.kivel@lightsource.ca>
2024-12-16 20:31:16 -0600

    fmt

--- src/bin/day14_1.rs
+++ src/bin/day14_1.rs
@@ -7,12 +7,16 @@ mod advent_of_code;
 struct Robot {
     position: (isize, isize),
     velocity: (isize, isize),
-    grid_size: (isize, isize)
+    grid_size: (isize, isize),
 }
 
 impl Robot {
     fn new(position: (isize, isize), velocity: (isize, isize), grid_size: (isize, isize)) -> Self {
-        Self { position, velocity, grid_size }
+        Self {
+            position,
+            velocity,
+            grid_size,
+        }
     }
 
     fn from_string(s: &String, grid_size: (isize, isize)) -> Self {
@@ -51,17 +55,18 @@ impl Robot {
         // Move the robot
         self.position.0 += time * self.velocity.0;
         self.position.1 += time * self.velocity.1;
-    
+
         // Correctly wrap around using mathematical modulo
-        self.position.0 = ((self.position.0 % self.grid_size.0) + self.grid_size.0) % self.grid_size.0;
-        self.position.1 = ((self.position.1 % self.grid_size.1) + self.grid_size.1) % self.grid_size.1;
+        self.position.0 =
+            ((self.position.0 % self.grid_size.0) + self.grid_size.0) % self.grid_size.0;
+        self.position.1 =
+            ((self.position.1 % self.grid_size.1) + self.grid_size.1) % self.grid_size.1;
     }
 }
 
 fn count_robots_in_quadrants(robots: Vec<Robot>) -> [isize; 4] {
-
-    let center_x = robots[0].grid_size.0/2;  // Vertical center line
-    let center_y = robots[0].grid_size.1/2; // Horizontal center line
+    let center_x = robots[0].grid_size.0 / 2; // Vertical center line
+    let center_y = robots[0].grid_size.1 / 2; // Horizontal center line
 
     println!("center x = {}, y = {}", center_x, center_y);
 
@@ -93,7 +98,10 @@ fn count_robots_in_quadrants(robots: Vec<Robot>) -> [isize; 4] {
 
 fn day14_1(data: &Vec<String>, grid_size: (isize, isize)) -> usize {
     // let grid_size = (11,7);
-    let mut robots: Vec<Robot> = data.iter().map(|line| Robot::from_string(line, grid_size)).collect();
+    let mut robots: Vec<Robot> = data
+        .iter()
+        .map(|line| Robot::from_string(line, grid_size))
+        .collect();
 
     robots.iter_mut().for_each(|robot| {
         robot.move_robot(100);
@@ -118,14 +126,14 @@ mod tests {
 
     #[test]
     fn robot_from_string_test() {
-        let robot = Robot::from_string(&"p=0,4 v=3,-3".to_owned(), (11,7));
-        assert_eq!(robot, Robot::new((0, 4), (3, -3), (11,7)));
+        let robot = Robot::from_string(&"p=0,4 v=3,-3".to_owned(), (11, 7));
+        assert_eq!(robot, Robot::new((0, 4), (3, -3), (11, 7)));
     }
 
     //p=2,4 v=2,-3
     #[test]
     fn robot_move_test() {
-        let mut robot = Robot::from_string(&"p=2,4 v=2,-3".to_owned(), (11,7));
+        let mut robot = Robot::from_string(&"p=2,4 v=2,-3".to_owned(), (11, 7));
         // 1 second
         robot.move_robot(1);
         assert_eq!(robot.position, (4, 1));
@@ -142,7 +150,7 @@ mod tests {
         robot.move_robot(1);
         assert_eq!(robot.position, (1, 3));
         // move 5 second in one go
-        let mut robot = Robot::from_string(&"p=2,4 v=2,-3".to_owned(), (11,7));
+        let mut robot = Robot::from_string(&"p=2,4 v=2,-3".to_owned(), (11, 7));
         robot.move_robot(5);
         assert_eq!(robot.position, (1, 3));
     }
@@ -150,7 +158,7 @@ mod tests {
     #[test]
     fn res_test() {
         let d = advent_of_code::Reader::read_file("./input/day14_test.txt").unwrap();
-        let result = day14_1(&d, (11,7));
+        let result = day14_1(&d, (11, 7));
         println!("result: {result}");
         assert_eq!(result, 12);
     }
@@ -158,7 +166,7 @@ mod tests {
     #[test]
     fn res_final() {
         let d = advent_of_code::Reader::read_file("./input/day14.txt").unwrap();
-        let result = day14_1(&d, (101,103));
+        let result = day14_1(&d, (101, 103));
         println!("result: {result}");
         assert_eq!(result, 236628054);
     }
